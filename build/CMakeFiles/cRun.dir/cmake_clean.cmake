3360132ef495f9880eea5c0dcb715355d2a39639
rdeka <raktimdeka14@gmail.com>
2024-08-11 12:08:11 +0530

    initial commit

---
+++ build/CMakeFiles/cRun.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cRun.dir/src/cRun.cpp.o"
+  "CMakeFiles/cRun.dir/src/cRun.cpp.o.d"
+  "cRun"
+  "cRun.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cRun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
