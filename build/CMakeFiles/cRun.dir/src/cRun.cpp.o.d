9dd80c37697468ccb040eba857731637f89631e6
rdeka <raktimdeka14@gmail.com>
2024-09-07 17:36:11 +0530

    forgot which feature

--- build/CMakeFiles/cRun.dir/src/cRun.cpp.o.d
+++ build/CMakeFiles/cRun.dir/src/cRun.cpp.o.d
@@ -39,4 +39,20 @@ CMakeFiles/cRun.dir/src/cRun.cpp.o: \
  /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
  /usr/include/bits/stdlib-float.h /usr/include/c++/14/bits/std_abs.h \
  /usr/include/c++/14/cstring /usr/include/c++/14/bits/version.h \
- /usr/include/string.h /usr/include/strings.h
+ /usr/include/string.h /usr/include/strings.h /usr/include/sys/wait.h \
+ /usr/include/signal.h /usr/include/bits/signum-generic.h \
+ /usr/include/bits/signum-arch.h /usr/include/bits/types/sig_atomic_t.h \
+ /usr/include/bits/types/siginfo_t.h /usr/include/bits/types/__sigval_t.h \
+ /usr/include/bits/siginfo-arch.h /usr/include/bits/siginfo-consts.h \
+ /usr/include/bits/siginfo-consts-arch.h \
+ /usr/include/bits/types/sigval_t.h /usr/include/bits/types/sigevent_t.h \
+ /usr/include/bits/sigevent-consts.h /usr/include/bits/sigaction.h \
+ /usr/include/bits/sigcontext.h /usr/include/bits/types/stack_t.h \
+ /usr/include/sys/ucontext.h /usr/include/bits/sigstack.h \
+ /usr/include/bits/sigstksz.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/linux/close_range.h /usr/include/bits/ss_flags.h \
+ /usr/include/bits/types/struct_sigstack.h /usr/include/bits/sigthread.h \
+ /usr/include/bits/signal_ext.h /usr/include/bits/types/idtype_t.h
--- src/cRun.cpp
+++ src/cRun.cpp
@@ -12,14 +12,64 @@ public:
     this->status = 0;
     this->child_pid = fork();
   }
-  void create() {
+  void create(char* bin, char** args) {
     if (this->child_pid == -1) exit(EXIT_FAILURE);
-    else if (this->child_pid == 0) 
-        execv
+    else if (this->child_pid == 0) {
+      if (execvp(bin, args) == -1) { perror("execvp"); exit(EXIT_FAILURE); }
+    } 
+    else 
+      waitpid(this->child_pid, &this->status, 0);
+  }
+  
+  void show_status() {
+    printf("Child Process ID: %d, Status: %d\n", this->child_pid, this->status);
+  }
+};
+
+class ccp2cp {
+private:
+  const char* cs;
+  char* ncs;
+public:
+  ccp2cp(const char* cs) { 
+    this->cs = cs; 
+    this->ncs = new char[strlen(this->cs) + 1]; 
+    strcpy(ncs, this->cs);
   }
 
+  char* covert2cp() { return ncs; }
+
+  int get_args_len() {
+    int wc = 0;
+    /* counting the number of arguments */
+    for(int i = 0; i < (int)strlen(this->cs); i++)
+      this->cs[i] == ' ' ? wc += 1 : wc += 0;
+    wc++; //count for the last argument 
+    return wc;
+  }
+
+  char** convert2cpp() {
+    int wc = get_args_len(); 
+
+    char** args = new char*[wc];
+    int args_index = 0;
+
+    char* buffer;
+    for(int i = 0; i < wc; i++) {
+      char current_char = this->cs[i];
+      if (current_char == ' ') {
+        args[args_index] = new char[strlen(buffer) + 1];
+        strcpy(args[args_index], buffer);
+        strtok(buffer, " ");
+      }
+
+    }
+  }
+  ~ccp2cp() { free(this->ncs); }
 };
 
 int main() {
+  ccp2cp s("raktimdeka arijitsadhukhan shriyaditadeka rajnandinichouny");
+  char** as = s.convert2cpp();
   return 0;
 }
